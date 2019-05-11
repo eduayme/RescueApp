15fdcf2c1e321da729c5389c7bc31dc3bc52b821
Edu Aymerich <eduardaymerich@gmail.com>
2019-05-11 20:22:52 +0200

    Added recerques migration

---
+++ database/migrations/2019_05_11_111351_create_recerques_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecerquesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recerques', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('es_practica')->default(0);
+            $table->string('num_actuacio')->unique();
+            $table->string('estat');
+
+            $table->dateTime('data_inici');
+            $table->dateTime('data_finalitzacio');
+            $table->dateTime('data_creacio');
+            $table->dateTime('data_ultima_modificacio');
+            $table->dateTime('data_tancament')->nullable();
+
+            $table->integer('id_usuari_creacio')->unsigned();
+            $table->integer('id_usuari_ultima_modificacio')->unsigned();
+            $table->integer('id_usuari_tancament')->unsigned();
+
+            $table->timestamps();
+
+            $table->foreign('id_usuari_creacio')->references('id')->on('users');
+            $table->foreign('id_usuari_ultima_modificacio')->references('id')->on('users');
+            $table->foreign('id_usuari_tancament')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recerques');
+    }
+}
