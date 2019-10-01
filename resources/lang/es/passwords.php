19141f1cd0c6f642673c51beb498827a28c27222
Eduard Aymerich <eduardaymerich@gmail.com>
2019-10-01 02:26:25 +0200

    Update passwords.php

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -2,8 +2,8 @@
 
 return [
     'password' => 'La contraseña debe tener como mínimo 6 caracteres y coincidir con la confirmación.',
-    'reset'    => "La contraseña se ha restablecido!",
-    'sent'     => "Se ha enviado un correo para poder restablecer la contraseña",
+    'reset'    => 'La contraseña se ha restablecido!',
+    'sent'     => 'Se ha enviado un correo para poder restablecer la contraseña',
     'token'    => 'La clave para restablecer la contraseña es inválida.',
-    'user'     => "No podemos encontrar el usuario con este email.",
+    'user'     => 'No podemos encontrar el usuario con este email.',
 ];
