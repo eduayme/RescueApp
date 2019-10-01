f1cce06ee00a3ab937622e2798305e1de0c6fc9c
Ricardo <rferromoreno@gmail.com>
2019-10-01 00:33:49 -0300

    Add English translation

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'required'        => 'This field is required.',
+    'min'             => 'This field must have at least 2 characters.',
+    'min8'            => 'This field must have at least 8 characters',
+    'max'             => 'This field cannot have more than 50 characters.',
+    'email'           => 'This field must have an email format.',
+    'confirmed'       => 'This field does not match the confirmation.',
+    'unique'          => 'This field must be unique in the data table.',
+    'numeric'         => 'This field must be a valid number.',
+    'updated'         => 'It has been updated successfully.',
+    'added'           => 'It has been added successfully.',
+    'deleted'         => 'It has been deleted successfully',
+    'not_allowed'     => 'You do not have permissions to do this action.',
+    'no_searches'     => 'No Search has been registered.',
+    'no_practices'    => 'No Practices has been registered',
+    'are_you_sure'    => 'Are you sure? This action can not be undone.',
+    'num_unique'      => 'The number must be unique in the data table.',
+    'closed_search'   => 'This Search is currently closed.',
+    'closed_practice' => 'This Practice is currently closed.',
+];
