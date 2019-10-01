d6e7fff3f610ca55444881bf25d86d8a5c8e338a
Andrea <andrea.laguillo@externo.cnic.es>
2019-10-01 10:30:42 +0200

    English translation
    Added English translations for all files in ./resources/lang

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'required'        => 'This field is required.',
+    'min'             => 'This field must contain at least 2 characters.',
+    'min8'            => 'This field must contain at least 8 characters.',
+    'max'             => 'This field must contain less than 50 characters.',
+    'email'           => 'This field must contain an email address',
+    'confirmed'       => 'This field does not match the confirmation.',
+    'unique'          => 'This field must be unique in the data table.',
+    'numeric'         => 'This field must contain a valid numeric value.',
+    'updated'         => 'Updated successfully.',
+    'added'           => 'Added successfully.',
+    'deleted'         => 'Deleted successfully.',
+    'not_allowed'     => 'You do not have permission to perform this action.',
+    'no_searches'     => 'No searches have been registered.',
+    'no_practices'    => 'No practices have been registered.',
+    'are_you_sure'    => 'Are you sure? This action cannot be undone.',
+    'num_unique'      => 'This number must be unique in the data table.',
+    'closed_search'   => 'This investigation is currently closed.',
+    'closed_practice' => 'This practice is currently closed.',
+];
