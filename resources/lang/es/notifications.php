a7790ed3f4da3a1fb34e384af70075d50cbf9fe5
Eduard Aymerich <eduardaymerich@gmail.com>
2019-10-01 02:25:46 +0200

    Update notifications.php

--- resources/lang/es/notifications.php
+++ resources/lang/es/notifications.php
@@ -6,6 +6,6 @@ return [
     'reset_pas'     => 'Restaurar contraseña',
     'reset_exp'     => 'Este link para restaurar la contraseña será válido únicamente durante :count minutos.',
     'reset_not_req' => 'Si no has solicitado esta restauración no necesitas realizar ninguna más acción, simplemente ignora este mensaje.',
-    'reset_send'    => "Se ha enviado un correo para restaurar la contraseña",
+    'reset_send'    => 'Se ha enviado un correo para restaurar la contraseña',
     'send_link'     => 'Enviar link',
 ];
