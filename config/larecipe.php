ab9f0634da6d5f5794c8b219e6c588e092a9a6dd
Eduard Aymerich <eduardaymerich@gmail.com>
2019-10-24 00:22:36 +0200

    Added Description in documentation

--- config/larecipe.php
+++ config/larecipe.php
@@ -16,7 +16,7 @@ return [
     'docs'        => [
         'route'   => '/docs',
         'path'    => '/resources/docs',
-        'landing' => 'overview',
+        'landing' => 'description',
         'middleware' => ['web'],
     ],
 
--- resources/docs/1.0/index.md
+++ resources/docs/1.0/index.md
@@ -1,2 +1,3 @@
 - ## Get Started
-    - [Overview](/{{route}}/{{version}}/overview)
+    - [Description](/{{route}}/{{version}}/description)
+    - [Installation](/{{route}}/{{version}}/installation)
