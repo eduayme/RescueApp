049cbcbaa864fd1cc21cfb8fac01a29639278a97
Dhaval <dhavalchheda@ymail.com>
2020-02-26 23:03:22 +0800

    Refactoring code.

--- resources/views/vendor/pagination/simple-bootstrap-4.blade.php
+++ resources/views/vendor/pagination/simple-bootstrap-4.blade.php
@@ -1,6 +1,7 @@
+<!-- Simple previous and next page link : OPEN -->
 @if ($paginator->hasPages())
     <ul class="pagination" role="navigation">
-        {{-- Previous Page Link --}}
+        <!-- Previous Page Link : OPEN -->
         @if ($paginator->onFirstPage())
             <li class="page-item disabled" aria-disabled="true">
                 <span class="page-link">@lang('pagination.previous')</span>
@@ -10,8 +11,8 @@
                 <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a>
             </li>
         @endif
-
-        {{-- Next Page Link --}}
+        <!-- Previous Page Link : CLOSE -->
+        <!-- Next Page Link : OPEN -->
         @if ($paginator->hasMorePages())
             <li class="page-item">
                 <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a>
@@ -21,5 +22,7 @@
                 <span class="page-link">@lang('pagination.next')</span>
             </li>
         @endif
+        <!-- Next Page Link : CLOSE -->
     </ul>
 @endif
+<!-- Simple previous and next page link : CLOSE -->
