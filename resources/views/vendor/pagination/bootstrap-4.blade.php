a7641905c892c4876de1ce25f2be0bb6dfd8c670
Dhaval <dhavalchheda@ymail.com>
2020-02-26 21:46:40 +0800

    Unwrapping some lines on code into multiple lines.

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -17,7 +17,9 @@
         @foreach ($elements as $element)
             <!-- "Three Dots" Separator : OPEN -->
             @if (is_string($element))
-                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                <li class="page-item disabled" aria-disabled="true">
+                    <span class="page-link">{{ $element }}</span>
+                </li>
             @endif
             <!-- "Three Dots" Separator : CLOSE -->
 
@@ -25,9 +27,13 @@
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                        <li class="page-item active" aria-current="page">
+                            <span class="page-link">{{ $page }}</span>
+                        </li>
                     @else
-                        <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                        </li>
                     @endif
                 @endforeach
             @endif
