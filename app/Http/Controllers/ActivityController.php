7b8b94f1f69cbe1db86041bf955783426e082036
Dhaval <dhavalchheda@ymail.com>
2020-02-26 16:01:10 +0800

    Refactoring of ActivityController
    Removal of unnecessary comments. 
    Removal of unnecessary variables and removal of the import statement.
    Following the indentation after each variable is declared. 
    Fixing the indentation of the comment.
    Addition of the comment for deleteAll function.

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Activity;
 use Illuminate\Http\Request;
 use App\User;
-use Auth;
 use File;
 use Image;
 use Validator;
@@ -13,36 +12,39 @@ use Gate;
 
 class ActivityController extends Controller
 {
-        /**
+    /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return void
      */
     public function index()
     {
-        // get user
-        $user = Auth::user();
+        /* Allow user to access activity logs of users if user is admin */
         if (Gate::allows('admin-only', auth()->user())) {
             $activities = Activity::orderBy('id', 'desc')->paginate(10);
-            //return $activities;
+
             return view('activities.index', compact('activities'));
         }
-     return back()->with('error', __('messages.not_allowed'));
+        return back()->with('error', __('messages.not_allowed'));
 
     }
 
+    /**
+     * Deleting all the records in activity log.
+     *
+     * @return void
+     */
     public function deleteAll()
     {
-        // get user
-        $user = Auth::user();
+
+        /* Allow user to delete all activity logs of users if user is admin */
         if (Gate::allows('admin-only', auth()->user())) {
             Activity::query()->delete();
             $activities = Activity::orderBy('id', 'desc')->paginate(10);
-            $error = __('messages.deleted');
-            //return $activities;
+
             return back()->with('error', __('main.activity_log') .  __('messages.deleted'));
         }
-     return back()->with('error', __('messages.not_allowed'));
+        return back()->with('error', __('messages.not_allowed'));
 
     }
 }
