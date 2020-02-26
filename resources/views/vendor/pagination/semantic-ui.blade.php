ac90271f2b6bc0f61b6fb8d2ae837663b82bf750
Dhaval <dhavalchheda@ymail.com>
2020-02-26 22:52:29 +0800

    Refactoring of Semantic UI Pagination.

--- resources/views/vendor/pagination/semantic-ui.blade.php
+++ resources/views/vendor/pagination/semantic-ui.blade.php
@@ -1,20 +1,25 @@
+<!-- This code renders the semantic iu pagination : OPEN -->
 @if ($paginator->hasPages())
     <div class="ui pagination menu" role="navigation">
-        {{-- Previous Page Link --}}
+        <!-- Previous Page Link : OPEN -->
         @if ($paginator->onFirstPage())
-            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
+            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                <i class="left chevron icon"></i>
+            </a>
         @else
-            <a class="icon item" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
+            <a class="icon item" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
+                <i class="left chevron icon"></i>
+            </a>
         @endif
-
-        {{-- Pagination Elements --}}
+        <!-- Previous Page Link : CLOSE -->
+        <!-- Pagination Elements : OPEN -->
         @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
+            <!-- "Three Dots" Separator : OPEN -->
             @if (is_string($element))
                 <a class="icon item disabled" aria-disabled="true">{{ $element }}</a>
             @endif
-
-            {{-- Array Of Links --}}
+            <!-- "Three Dots" Separator : CLOSE -->
+            <!-- Array Of Links  : OPEN -->
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
@@ -24,13 +29,21 @@
                     @endif
                 @endforeach
             @endif
+            <!-- Array Of Links  : CLOSE -->
         @endforeach
+        <!-- Pagination Elements : CLOSE -->
 
-        {{-- Next Page Link --}}
+        <!-- Next Page Link : OPEN -->
         @if ($paginator->hasMorePages())
-            <a class="icon item" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')"> <i class="right chevron icon"></i> </a>
+            <a class="icon item" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
+                <i class="right chevron icon"></i>
+            </a>
         @else
-            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.next')"> <i class="right chevron icon"></i> </a>
+            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                <i class="right chevron icon"></i>
+            </a>
         @endif
+        <!-- Next Page Link : CLOSE -->
     </div>
 @endif
+<!-- This code renders the semantic iu pagination : CLOSE -->
