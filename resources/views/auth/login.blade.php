e987397221dfd409bd00c2479cf04bafa7639994
Eduard Aymerich <eduardaymerich@gmail.com>
2020-05-05 20:56:40 +0200

    Improved login page style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,10 +11,10 @@
     <!-- Content - OPEN -->
     <div class="container margin-top padding-bottom">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-6">
 
                 <!-- Card - OPEN -->
-                <div class="card">
+                <div class="card shadow">
 
                     <!-- Card header - OPEN -->
                     <div class="card-header">
@@ -30,98 +30,79 @@
                             @csrf
 
                             <!-- Email - OPEN -->
-                            <div class="form-group row">
+                            <div class="form-group">
 
                                 <!-- Email label - OPEN -->
-                                <label for="email" class="col-md-4 col-form-label text-md-right">
+                                <label for="email" class="col-form-label text-md-right">
                                     {{ __('login.email') }}
                                 </label>
                                 <!-- Email label - CLOSE -->
 
                                 <!-- Email input - OPEN -->
-                                <div class="col-md-6">
-
-                                    <!-- Input - OPEN -->
-                                    <input id="email" type="email" name="email" value="{{ old('email') }}"
-                                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required autofocus>
-                                    <!-- Input - CLOSE -->
-
-                                    <!-- Show errors input - OPEN -->
-                                    @if( $errors->has('email') )
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong> {{ $errors->first('email') }} </strong>
-                                        </span>
-                                    @endif
-                                    <!-- Show errors input - CLOSE -->
-
-                                </div>
+                                <input id="email" type="email" name="email" value="{{ old('email') }}"
+                                class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required autofocus>
                                 <!-- Email input - CLOSE -->
+
+                                <!-- Show errors input - OPEN -->
+                                @if( $errors->has('email') )
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong> {{ $errors->first('email') }} </strong>
+                                    </span>
+                                @endif
+                                <!-- Show errors input - CLOSE -->
 
                             </div>
                             <!-- Email - CLOSE -->
 
                             <!-- Password - OPEN -->
-                            <div class="form-group row">
+                            <div class="form-group">
 
                                 <!-- Password label - OPEN -->
-                                <label for="password" class="col-md-4 col-form-label text-md-right">
+                                <label for="password" class="col-form-label text-md-right">
                                     {{ __('login.password') }}
                                 </label>
                                 <!-- Password label - CLOSE -->
 
                                 <!-- Password input - OPEN -->
-                                <div class="col-md-6">
-
-                                    <!-- Input - OPEN -->
-                                    <input id="password" type="password" name="password"
-                                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required>
-                                    <!-- Input - CLOSE -->
-
-                                    <!-- Show errors input - OPEN -->
-                                    @if( $errors->has('password') )
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong> {{ $errors->first('password') }} </strong>
-                                        </span>
-                                    @endif
-                                    <!-- Show errors input - CLOSE -->
-
-                                </div>
+                                <input id="password" type="password" name="password"
+                                class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required>
                                 <!-- Password input - CLOSE -->
+
+                                <!-- Show errors input - OPEN -->
+                                @if( $errors->has('password') )
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong> {{ $errors->first('password') }} </strong>
+                                    </span>
+                                @endif
+                                <!-- Show errors input - CLOSE -->
 
                             </div>
                             <!-- Password - CLOSE -->
 
                             <!-- Remember me - OPEN -->
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
+                            <div class="form-group form-check margin-top">
 
-                                        <!-- Remember me checkbox - OPEN -->
-                                        <input class="form-check-input" id="remember" type="checkbox"
-                                        name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        <!-- Remember me checkbox - CLOSE -->
+                                <!-- Remember me checkbox - OPEN -->
+                                <input class="form-check-input" id="remember" type="checkbox"
+                                name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <!-- Remember me checkbox - CLOSE -->
 
-                                        <!-- Remember me label - OPEN -->
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('login.remember') }}
-                                        </label>
-                                        <!-- Remember me label - CLOSE -->
-                                    </div>
-                                </div>
+                                <!-- Remember me label - OPEN -->
+                                <label class="form-check-label" for="remember">
+                                    {{ __('login.remember') }}
+                                </label>
+                                <!-- Remember me label - CLOSE -->
+
                             </div>
                             <!-- Remember me - CLOSE -->
 
                             <!-- Login button - OPEN -->
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-
-                                    <!-- Button - OPEN -->
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('login.login') }}
-                                    </button>
-                                    <!-- Button - CLOSE -->
-
-                                </div>
+                            <div class="form-group">
+                                <!-- Button - OPEN -->
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    {{ __('login.login') }}
+                                </button>
+                                <!-- Button - CLOSE -->
                             </div>
                             <!-- Login button - CLOSE -->
 
