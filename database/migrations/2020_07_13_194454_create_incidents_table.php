4b178a2c9bbb6cbb526664c6365540351d37eef4
Eduard Aymerich <eduardaymerich@gmail.com>
2020-07-13 19:51:53 +0200

    Added incidents tables

---
+++ database/migrations/2020_07_13_194454_create_incidents_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncidentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_search')->unsigned();
+            $table->integer('id_user_creation')->unsigned();
+            $table->integer('id_user_modification')->unsigned();
+            $table->dateTime('date')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('incidents', function (Blueprint $table) {
+            $table->foreign('id_search')->references('id')->on('searches')->onDelete('cascade');
+            $table->foreign('id_user_creation')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_user_modification')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidents');
+    }
+}
---
+++ database/migrations/2020_07_13_194935_create_incidents_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncidentsImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidents_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_incident')->unsigned();
+            $table->integer('id_user_updated')->unsigned();
+            $table->string('photo')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('incidents_images', function (Blueprint $table) {
+            $table->foreign('id_incident')->references('id')->on('incidents')->onDelete('cascade');
+            $table->foreign('id_user_updated')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidents_images');
+    }
+}
