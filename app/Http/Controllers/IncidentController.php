271b0f39585bc6bfff443361b495766c64683f9f
Eduard Aymerich <eduardaymerich@gmail.com>
2020-07-27 21:35:29 +0200

    Incident Controller created

---
+++ app/Http/Controllers/IncidentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Incident;
+use Illuminate\Http\Request;
+
+class IncidentController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+    }
+}
