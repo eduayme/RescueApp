7ba27f441e6e706a2bd9b581a30ace0ca6d934f1
Florent BESNARD <florent.besnard@outlook.com>
2020-10-04 01:59:59 +0200

    French translation

---
+++ resources/lang/fr/profile_lost_person.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'development_disorder'            => 'Trouble du développement',
+    'alzheimer_or_other_dementias'    => 'Alzheimer ou autres démences',
+    'mental_or_psychological_illness' => 'Troubles psychologiques',
+    'autolithic_behaviors'            => 'Comportements autolithiques',
+    'hiker'                           => 'Randonneur',
+    'collector'                       => 'Collectionneur'
+    'mushroom_finder'                 => 'Chercheur de champignons',
+    'none_of_the_above'               => 'Aucun de ces éléments',
+];
