369419cba1f21556080bb8f71952ced6f8ca34fd
Eduard Aymerich <eduardaymerich@gmail.com>
2020-10-04 00:01:27 +0200

    bug: Fix profile lost person types #147

--- resources/lang/en/profile_lost_person.php
+++ resources/lang/en/profile_lost_person.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'development_disorder'            => 'Development_disorder',
+    'development_disorder'            => 'Development disorder',
     'alzheimer_or_other_dementias'    => 'Alzheimer or other dementias',
     'mental_or_psychological_illness' => 'Mental or psychological illness',
     'autolithic_behaviors'            => 'Autolithic behaviors',
     'hiker'                           => 'Hiker',
+    'collector'                       => 'Collector'
     'mushroom_finder'                 => 'Mushroom finder',
     'none_of_the_above'               => 'None of the above',
 ];
