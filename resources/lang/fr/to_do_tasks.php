79b05c0c64ee3b6a33a603715f185c9676130a24
gauritiwari <72256767+gauritiwari@users.noreply.github.com>
2020-10-04 14:00:04 +0530

    Update to_do_tasks.php

--- resources/lang/fr/to_do_tasks.php
+++ resources/lang/fr/to_do_tasks.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'BD_personal'  => 'BD personnelle (pièce d'identité, permis de conduire, ...)',
+    'BD_personal'  => 'BD personnelle (pièce d identité, permis de conduire, ...)',
     'BD_hospitals' => 'Hôpitaux BD',
     'Phone_LP'     => 'Dernière position du téléphone',
     'IMEI_LP'      => 'Dernière position du téléphone IMEI',
