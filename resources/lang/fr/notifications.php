b16cc66da1347ed9191ad290ff73a7c753b742ae
gauritiwari <72256767+gauritiwari@users.noreply.github.com>
2020-10-04 13:55:44 +0530

    Update notifications.php

--- resources/lang/fr/notifications.php
+++ resources/lang/fr/notifications.php
@@ -5,7 +5,7 @@ return [
     'receiving'     => 'Vous recevez cet e-mail car vous avez demandé la restauration de votre mot de passe.',
     'reset_pas'     => 'Restaurer le mot de passe',
     'reset_exp'     => 'Ce lien de restauration de votre mot de passe sera valide pendant: compter minutes seulement.',
-    'reset_not_req' => 'Si vous n'avez pas demandé la restauration de votre mot de passe, vous n'avez pas besoin de prendre d'autres mesures, ignorez simplement ce message.',
+    'reset_not_req' => 'Si vous n avez pas demandé la restauration de votre mot de passe, vous n avez pas besoin de prendre d autres mesures, ignorez simplement ce message.',
     'reset_send'    => 'Un e-mail pour restaurer votre mot de passe a été envoyé',
     'send_link'     => 'Envoyer un lien',
 ];
