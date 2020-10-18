775907eed0ee4cced4918a186bd6ba04c664e390
Eduard Aymerich <eduardaymerich@gmail.com>
2020-10-18 18:58:58 +0200

    refactor: Groups language translation

---
+++ resources/lang/ca/group.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'group'                          => 'Grup',
+    'id'                             => 'Id',
+    'status'                         => 'Estat',
+    'vehicle'                        => 'Vehicle',
+    'broadcast'                      => 'Transmissió',
+    'gps'                            => 'GPS',
+    'people_involved'                => 'Persones involucrades',
+    'is_active'                      => 'Actiu',
+    'is_closed'                      => 'Inactiu',
+    'lengthMenu'                     => 'Mostrar _MENU_ grups',
+    'group_add_success'              => ' afegit correctament.',
+    'group_edit_success'             => ' editat correctament.',
+    'group_delete_success'           => ' eliminat correctament.',
+    'group_not_found'                => ' no trobat.',
+];
---
+++ resources/lang/es/group.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'group'                          => 'Grupo',
+    'id'                             => 'Id',
+    'status'                         => 'Estado',
+    'vehicle'                        => 'Vehiculo',
+    'broadcast'                      => 'Transmisión',
+    'gps'                            => 'GPS',
+    'people_involved'                => 'Personas involucradas',
+    'is_active'                      => 'Activo',
+    'is_closed'                      => 'Inactivo',
+    'lengthMenu'                     => 'Mostrar _MENU_ grupos',
+    'group_add_success'              => ' añadido correctamente.',
+    'group_edit_success'             => ' editado correctamente.',
+    'group_delete_success'           => ' eliminado correctamente.',
+    'group_not_found'                => ' no encontrado.',
+];
