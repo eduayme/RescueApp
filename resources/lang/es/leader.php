c25feeaf4666682fba013556bd5d2a2239e50a34
Eduard Aymerich <eduardaymerich@gmail.com>
2020-11-02 18:53:30 +0100

    enh: Improved Leader translations

---
+++ resources/lang/es/leader.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'leader'                      => 'Líder',
+    'id'                          => 'Id',
+    'name'                        => 'Nombre',
+    'phone'                       => 'Teléfono',
+    'start'                       => 'Inicio',
+    'end'                         => 'Final',
+    'lengthMenu'                  => 'Mostrar _MENU_ líderes',
+    'leader_add_success'          => ' añadido correctamente.',
+    'leader_edit_success'         => ' editado correctamente.',
+    'leader_delete_success'       => ' eliminado correctamente.',
+    'leader_not_found'            => ' no encontrado.',
+];
