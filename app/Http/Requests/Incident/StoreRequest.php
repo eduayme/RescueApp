9fa5393a1f518ceec018e66328b1079ebb046389
Soneye Sina <advancoplanet@gmail.com>
2022-03-19 01:00:57 +0100

    IncidentController Refactor

---
+++ app/Http/Requests/Incident/StoreRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Incident;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'description' => 'required',
+            'date'        => 'required',
+            'images'       => 'mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'user_creation_id' => 'integer|required',
+            'user_modification_id' => 'integer|required',
+            'search_id' => 'required|integer'
+        ];
+    }
+
+    /**
+     * Validation messages
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'description.required'  => __('messages.required'),
+            'date.required'         => __('messages.required'),
+            'images'                => __('messages.mimes'),
+            'images.size'           => __('messages.photo_max'),
+        ];
+    }
+}
---
+++ app/Http/Requests/Incident/UpdateRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Incident;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'description' => 'required',
+            'date'        => 'required|date',
+            'user_modification_id' => 'integer|sometimes',
+            'images_delete' => 'mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ];
+    }
+
+    /**
+     * Validation messages
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'description.required' => __('messages.required'),
+            'date.required'        => __('messages.required'),
+            'images_delete.mimes'          => __('messages.mimes'),
+            'images_delete.size'            => __('messages.photo_max'),
+        ];
+    }
+}
